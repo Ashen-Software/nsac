5c4b48aa7fabbe4eb506591469dcba129cca9c81
Melany <mefrancom@unal.edu.co>
2024-10-05 20:46:39 -0500

    about

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,17 @@
 import './home.css';
 import Explanation from '../../components/Explanation/Explanation';
 import Address from '../../components/Address/Address';
-
+import About from "../../components/About/About";
 
 function Home(){
     return(
         <>
-        <div className='home'>
-            <Explanation/>
-            <Address/>
+        <div className='seccion'>
+            <About/>
+            <div className='home'>
+                <Explanation/>
+                <Address/>
+            </div>
         </div>
 </>
     )
