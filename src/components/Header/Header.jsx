6c7e7b177f3d3674735ec514df3216e46ae78ad9
under_world <underworld.programmer@gmail.com>
2024-10-06 16:23:32 -0500

    scroll added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,39 @@
 import "./header.css";
 import logo from "../../assets/NSAC logo.png"
+import {useNavigate} from "react-router-dom";
+import { scroller } from 'react-scroll';
 
 function Header(){
+
+    const navigate = useNavigate();
+
+    const handleNavigation = (target) => {
+        if (window.location.pathname !== "/") {
+            navigate("/", { replace: true });
+            setTimeout(() => {
+                scroller.scrollTo(target, {
+                    duration: 800,
+                    delay: 0,
+                    smooth: 'easeInOutQuart',
+
+                });
+            }, 100);  // Espera un poco para asegurarte de que la navegación se complete
+        } else {
+            scroller.scrollTo(target, {
+                duration: 800,
+                delay: 0,
+                smooth: 'easeInOutQuart'
+            });
+        }
+    };
 
     return(<div className="header">
 
         <img src={logo} alt = "" className="logo"/>
         <div className="tabs">
-            <p className="tab">Mars</p>
-            <p className="tab">Problematic</p>
-            <p className="tab">About us</p>
+            <p className="tab" >Mars</p>
+            <p className="tab" onClick={() => handleNavigation('carousel')}>Problematic</p>
+            <p className="tab" onClick={() => handleNavigation('about')}>About us</p>
         </div>
     </div>)
 }
