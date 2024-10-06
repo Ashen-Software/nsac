da35c6c4bc2e3ce4794fcdb86d2b674257967f28
Melany <mefrancom@unal.edu.co>
2024-10-06 08:11:55 -0500

    what is

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,11 @@
 import './home.css';
-
+import WhatIs from '../../components/WhatIs/WhatIs';
 
 function Home(){
     return(
         <div>
         <p>cero</p>
+        <WhatIs/>
 </div>
     )
 }
