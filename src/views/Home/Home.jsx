a8010291106af3ab9ffbab6de5c01e2558cd7d01
Melany <mefrancom@unal.edu.co>
2024-10-05 21:08:07 -0500

    logo

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,6 +2,8 @@ import './home.css';
 import Explanation from '../../components/Explanation/Explanation';
 import Address from '../../components/Address/Address';
 import About from "../../components/About/About";
+import logo from "../../assets/NSAC logo.png";
+
 
 function Home(){
     return(
@@ -9,6 +11,7 @@ function Home(){
         <p className='namazu'>NAMAZU</p>
         <div className='seccion'>
             <div className='izq'>
+                <img className="logo" src={logo} alt='space apps logo'/>
                 <About/>
             </div>
             <div className='home'>
